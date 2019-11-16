438a3959d561ee3a94981829190d6c06c6ede4fc
Lucian Amos <locamos@gmail.com>
2019-11-16 13:46:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,15 +12,10 @@ function whileLoop(n){
   return 'done';
 }
 
-var i = 0;
- 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
 
 
-function doWhileLoop(incrementVariable){
+
+function doWhileLoop(num){
   do { console.log("I run once regardless.");
   } while (incrementVariable() < num)
 }

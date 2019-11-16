d2d01861efd360baa1601bac19b66a620c7ce8a6
Lucian Amos <locamos@gmail.com>
2019-11-16 13:31:58 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+  for(let i = 0; i<25; i++){
+   array.push(`I am ${i} strange loop${i===1 ? '' : 's'}.`);
+    }
+  return array;
+}
+
+function whileLoop(n){
+  while(n>=0){
+    console.log(n);
+  }
+}
